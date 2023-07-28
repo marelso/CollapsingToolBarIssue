447f0fa28194ce4acf5555dad5f3744ddad4b57f
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-07-28 19:19:11 -0300

    Including viewBinding at the build gradle file and finishing adapter.

--- app/src/main/java/com/marelso/collapsingtoolbarissue/ui/FilterAdapter.kt
+++ app/src/main/java/com/marelso/collapsingtoolbarissue/ui/FilterAdapter.kt
@@ -1,25 +1,36 @@
 package com.marelso.collapsingtoolbarissue.ui
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.chip.Chip
+import com.marelso.collapsingtoolbarissue.data.Filter
+import com.marelso.collapsingtoolbarissue.databinding.ItemFilterBinding
+import java.lang.RuntimeException
 
-class FilterAdapter: RecyclerView.Adapter<FilterAdapter.ViewHolder>() {
+class FilterAdapter(private val dataList: List<Filter>) :
+    RecyclerView.Adapter<FilterAdapter.ViewHolder>() {
+
+    private var binding: ItemFilterBinding? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        this.binding = ItemFilterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return this.binding?.let { ViewHolder(it.root) } ?: throw RuntimeException("AA")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = dataList.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(dataList[position])
     }
 
-    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val chip: Chip? = binding?.filter
+        fun bind(filter: Filter) {
+            chip?.text = filter.name
+        }
     }
 
 }
